b4dbaa6938f5c7023184b3cb0c0dc94a146d1b94
onetrue5200 <oneruexie@gmail.com>
2022-09-28 00:35:45 +0800

    websocket for playing & record

---
+++ backend/src/main/java/top/onetrue/backend/mapper/RecordMapper.java
@@ -0,0 +1,9 @@
+package top.onetrue.backend.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import top.onetrue.backend.pojo.Record;
+
+@Mapper
+public interface RecordMapper extends BaseMapper<Record> {
+}
