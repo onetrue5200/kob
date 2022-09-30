4ba3455295a6b93dc03859f338c5985f3e73bab6
onetrue5200 <oneruexie@gmail.com>
2022-09-30 17:47:05 +0800

    compile & run the bot code

--- backendcloud/botrunningsystem/src/main/java/top/onetrue/botrunningsystem/BotRunningSystemApplication.java
+++ backendcloud/botrunningsystem/src/main/java/top/onetrue/botrunningsystem/BotRunningSystemApplication.java
@@ -2,10 +2,12 @@ package top.onetrue.botrunningsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import top.onetrue.botrunningsystem.service.impl.BotRunningServiceImpl;
 
 @SpringBootApplication
 public class BotRunningSystemApplication {
     public static void main(String[] args) {
+        BotRunningServiceImpl.botPool.start();
         SpringApplication.run(BotRunningSystemApplication.class, args);
     }
 }
--- backendcloud/botrunningsystem/src/main/java/top/onetrue/botrunningsystem/service/impl/BotRunningServiceImpl.java
+++ backendcloud/botrunningsystem/src/main/java/top/onetrue/botrunningsystem/service/impl/BotRunningServiceImpl.java
@@ -2,12 +2,15 @@ package top.onetrue.botrunningsystem.service.impl;
 
 import org.springframework.stereotype.Service;
 import top.onetrue.botrunningsystem.service.BotRunningService;
+import top.onetrue.botrunningsystem.service.impl.utils.BotPool;
 
 @Service
 public class BotRunningServiceImpl implements BotRunningService {
+    public final static BotPool botPool = new BotPool();
     @Override
     public String addBot(Integer userId, String botCode, String input) {
         System.out.println("add bot: " + userId + " " + botCode + " " + input);
+        botPool.addBot(userId, botCode, input);
         return "add bot success";
     }
 }
